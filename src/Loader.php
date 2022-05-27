7a5b8fa7ee8f3c17acffcd3738e1ee09eb65b825
Paige Jones <hello@paigejones.me>
2022-05-27 12:00:33 +0100

    feat: add custom loader

---
+++ src/Loader.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace LumiteStudios\JSONTranslations;
+
+use RuntimeException;
+use Illuminate\Translation\FileLoader;
+
+class Loader extends FileLoader
+{
+    /**
+     * Add a new JSON path to the loader.
+     *
+     * @param  string  $path
+     * @return void
+     */
+    public function addJsonPath($data)
+    {
+        $this->jsonPaths[$data['prefix']] = $data['path'];
+    }
+
+    /**
+     * Load a locale from the given JSON file path.
+     *
+     * @param  string  $locale
+     * @return array
+     *
+     * @throws \RuntimeException
+     */
+    protected function loadJsonPaths($locale)
+    {
+        return collect($this->jsonPaths)
+            ->reduce(function ($output, $path, $prefix) use ($locale) {
+                if ($this->files->exists($full = "{$path}/{$locale}.json")) {
+                    $decoded = json_decode($this->files->get($full), true);
+
+                    if (is_string($prefix)) {
+                        $decoded = collect($decoded)->mapWithKeys(function ($value, $key) use ($prefix) {
+                            return ["{$prefix}.{$key}" => $value];
+                        })->toArray();
+                    }
+
+                    if (is_null($decoded) || json_last_error() !== JSON_ERROR_NONE) {
+                        throw new RuntimeException("Translation file [{$full}] contains an invalid JSON structure.");
+                    }
+
+                    $output = array_merge($output, $decoded);
+                }
+
+                return $output;
+            }, []);
+    }
+}
--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -2,8 +2,37 @@
 
 namespace LumiteStudios\JSONTranslations;
 
+use LumiteStudios\JSONTranslations\Loader;
 use Illuminate\Translation\TranslationServiceProvider as ServiceProvider;
 
 class TranslationServiceProvider extends ServiceProvider
 {
+    /**
+     * The direct path.
+     * @var string
+     */
+    protected string $root = __DIR__ . '/..';
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->singleton('translation.loader', function ($app) {
+            return new Loader($app['files'], $app['path.lang']);
+        });
+    }
+
+    /**
+     * Register a JSON translation file path.
+     *
+     * @param  string  $path
+     * @return void
+     */
+    protected function loadJsonTranslationsWithPrefix($path, $prefix)
+    {
+        $this->app['translator']->addJsonPath(['prefix' => $prefix, 'path' => $path]);
+    }
 }
